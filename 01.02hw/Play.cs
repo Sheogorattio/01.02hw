cdd8138e4e579e5d48eba9c602b6c539dbdd61b9
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-03-03 11:48:30 +0200

    Add project files.

---
+++ 01.02hw/Play.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayClass
+{ 
+    public class Play : IDisposable
+    {
+        public string Name { set; get; }
+        public string Genre { set; get; }
+        public string Author { set; get; }
+        public int Year { set; get; }
+        public Play(string name, string genre, string author)
+        {
+            Name = name;
+            Genre = genre;
+            Author = author;
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            Console.WriteLine("Disposable was called");
+        }
+        ~Play()
+        {
+            Console.WriteLine("Destuctor was called");
+        }
+    }
+}
---
+++ 01.02hw/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlayClass;
+
+namespace _01._02hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Play p = new Play("Name", "Genre", "Author")) { }
+            Play _p = new Play("Name", "Genre", "Author");
+        }
+    }
+}
